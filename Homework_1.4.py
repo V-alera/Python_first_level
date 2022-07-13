22b88211b786307b5b487a5c0ad9115d6a893d4e
valeriy.osipchikov@gmail.com <prosoundmsk@mail.ru>
2022-07-13 19:03:57 +0300

    Python.Homework 1.4.

--- Homework_1.4.py
+++ Homework_1.4.py
@@ -1,3 +1,21 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат 
 # точек в этой четверти (x и y).
 
+import math
+
+
+def inputValue():
+    part = int(input(f"Введите значение четверти: "))
+    return part
+
+def checkPart(num):
+    if (num == 1): print (f"x > 0, y > 0")
+    elif (num == 2): print (f"x > 0, y < 0")
+    elif (num == 3): print (f"x < 0, y < 0")
+    else: print (f"x < 0, y > 0")
+
+partValue = None
+
+partValue = inputValue()
+
+checkPart(partValue)
