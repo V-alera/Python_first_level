949ab2f75cf8c89cc2d40b7e3ae8af3f8d653711
valeriy.osipchikov@gmail.com <prosoundmsk@mail.ru>
2022-08-08 19:49:42 +0300

    Python.Homework_8.1

---
+++ Homework_8.1/main.py
@@ -0,0 +1,18 @@
+
+database = {}
+
+def reading_file_to_dict(number_file):
+    with open (f'{number_file}.txt', 'r', encoding='utf-8') as file_1:
+        data = [i.split('\n')[0] for i in file_1.readlines()]
+        database[number_file] = []
+        for i in data:
+            database[number_file].append(tuple(i.split(';')))
+        print(database)
+
+def print_childrens (second_name):
+    pass
+
+reading_file_to_dict(1)
+reading_file_to_dict(2)
+print(database)
+print_childrens ('Иванов')
